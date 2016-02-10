0e657e617a241940b9676b73e60eee5bcc7843a0
BuildTools <unconfigured@null.spigotmc.org>
2016-02-10 17:25:18 -0600

    Test Server Plugin

---
+++ src/TestPlugin/FirstCommand.java
@@ -0,0 +1,25 @@
+package TestPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FirstCommand extends JavaPlugin{
+	@Override
+	public void onEnable(){
+		getLogger().info("WolfOfWallStreet has been enabled.");
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("PjSucks")) { // If the player typed /basic then do the following...
+			// doSomething
+			return true;
+		} //If this has happened the function will return true. 
+	        // If this hasn't happened the value of false will be returned.
+		return false; 
+	}
+	@Override
+	public void onDisable(){
+		getLogger().info("WolfOfWallStreet has been disabled.");
+	}
+}
