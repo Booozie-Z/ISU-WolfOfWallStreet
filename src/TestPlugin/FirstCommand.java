00f1e94c81c0c75483f4fd15d367257674e12747
BuildTools <unconfigured@null.spigotmc.org>
2016-02-10 18:10:16 -0600

    First Working Plugin Usage: /pjsucks

--- src/TestPlugin/FirstCommand.java
+++ src/TestPlugin/FirstCommand.java
@@ -7,12 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FirstCommand extends JavaPlugin{
 	@Override
 	public void onEnable(){
-		getLogger().info("WolfOfWallStreet has been enabled.");
+		getLogger().info("WolfOfWallStreet has been enabled.");//Sends Message in console when Plugin is loaded.
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("PjSucks")) { // If the player typed /basic then do the following...
-			// doSomething
+			sender.sendMessage("Yes he does.");
 			return true;
 		} //If this has happened the function will return true. 
 	        // If this hasn't happened the value of false will be returned.
